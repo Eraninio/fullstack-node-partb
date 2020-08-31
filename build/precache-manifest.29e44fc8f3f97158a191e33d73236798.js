070a73d6f3ffa7c9e3296b7d4757282b28f8c16e
Eraninio <67857483+Eraninio@users.noreply.github.com>
2020-08-31 17:47:25 +0300

    bla

---
+++ build/precache-manifest.29e44fc8f3f97158a191e33d73236798.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e31c8509d8a058363f8b31f10a2a4cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c267c7dbdc6336f4f074",
+    "url": "/static/css/main.adb0e0ae.chunk.css"
+  },
+  {
+    "revision": "261379cd12f7a257a30b",
+    "url": "/static/js/2.72903e02.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.72903e02.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c267c7dbdc6336f4f074",
+    "url": "/static/js/main.1f652d08.chunk.js"
+  },
+  {
+    "revision": "8dd7441d529e73d61d47",
+    "url": "/static/js/runtime-main.aaa24d6c.js"
+  }
+]);
